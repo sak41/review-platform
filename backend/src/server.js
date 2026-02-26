3207451fc9e4d34210679fc3edadd4c0734ba982
saksham yadav <sakyadav9936@gmail.com>
2026-02-27 01:18:09 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,9 +32,19 @@ import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express"
+import { inngest } from "./lib/inngest.js";
 
 const app = express();
 const __dirname = path.resolve();
+
+//middleware
+app.use(express.json());
+//CREDENTIALS TRUE MEANS SERVER ALLOW THE BROWSERS TO INCLUDE COOKIES ON REQ 
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+app.use("/api/inngest", serve({client : inngest, functions}));
 
 app.get("/books", (req, res) => {
   res.status(200).json({ msg: "api is running and book endpoint" });
