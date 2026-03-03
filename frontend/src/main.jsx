a824ec8fdcff47494a40753e759084f22266f357
saksham yadav <sakyadav9936@gmail.com>
2026-03-03 17:54:45 +0530

    dashboard page added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,12 +19,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
